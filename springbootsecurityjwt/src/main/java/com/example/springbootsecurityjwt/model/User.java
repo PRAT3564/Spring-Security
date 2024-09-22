96c925cdcc6172d5a56f0a5e60f6f9cc8f21bb18
Prathamesh Patil <126692529+PRAT3564@users.noreply.github.com>
2024-09-23 02:20:38 +0530

    Add files via upload

---
+++ springbootsecurityjwt/src/main/java/com/example/springbootsecurityjwt/model/User.java
@@ -0,0 +1,65 @@
+package com.example.springbootsecurityjwt.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "username"),
+                @UniqueConstraint(columnNames = "email")
+        })
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 20)
+    private String username;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+
+    @Column(nullable = false, length = 120)
+    private String password;
+
+    // Constructors, getters, and setters
+
+    public User(String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
